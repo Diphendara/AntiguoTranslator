2f6a96d41e000543cfbb6725a112ce26170dbc01
diphendara <cristiancvacas@gmail.com>
2019-03-24 15:38:03 +0100

    Finish number conversor

--- app/src/main/java/diphendara/antiguo/translator/fragments/NumberFragment.kt
+++ app/src/main/java/diphendara/antiguo/translator/fragments/NumberFragment.kt
@@ -2,10 +2,16 @@ package diphendara.antiguo.translator.fragments
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import diphendara.antiguo.translator.R
+import kotlinx.android.synthetic.main.number_fragment_tab1.*
+
+
 
 class NumberFragment : Fragment() {
 
@@ -13,4 +19,36 @@ class NumberFragment : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.number_fragment_tab1, container, false)
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        inputNumber.addTextChangedListener(object : TextWatcher {
+
+            override fun afterTextChanged(s: Editable) {
+                if(s.toString().isNotEmpty()) {
+                    try {
+                        antiguoNumberTextView.text = parseText(s.toString())
+                    } catch (exception: NumberFormatException) {
+                        Toast.makeText(context, getString(R.string.number_to_long), Toast.LENGTH_LONG).show()
+                    }
+                }
+            }
+
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {}
+        })
+    }
+
+    fun parseText(inputText: String): String {
+        val number: Long = inputText.toLong()
+        val quotient = number / 5
+        val remainder = number % 5
+
+        return if (quotient.compareTo(0) == 0){
+            remainder.toString()
+        } else {
+            parseText(quotient.toString()) + remainder.toString()
+        }
+    }
+
 }
