c3368e284ed33edc4e171540ca513c9ff2e62c12
diphendara <cristiancvacas@gmail.com>
2019-03-26 19:50:48 +0100

    code climate make know cases static funcion in know case object

--- app/src/main/java/diphendara/antiguo/translator/dataObjects/KnowCase.kt
+++ app/src/main/java/diphendara/antiguo/translator/dataObjects/KnowCase.kt
@@ -1,3 +1,31 @@
 package diphendara.antiguo.translator.dataObjects
 
-class KnowCase(var value: String, var antiguoValue: String)
+class KnowCase(var value: String, var antiguoValue: String) {
+
+    companion object {
+        fun getKnownWords(): Array<KnowCase>
+        {
+            // https://cincoelementos.fandom.com/es/wiki/Lenguaje_Antiguo#Diccionario
+            val knownWords = Array(14) { KnowCase("Agua", "HguH") }
+
+            // Read the README to know why this values are like this
+            knownWords[0] = KnowCase("agua", "HiW")
+            knownWords[1] = KnowCase("argumento", "Hrugumentu")
+            knownWords[2] = KnowCase("capitulo", "Cpitoru")
+            knownWords[3] = KnowCase("mujer", "DoN")
+            knownWords[4] = KnowCase("aire", "Er")
+            knownWords[5] = KnowCase("espacio", "esPi")
+            knownWords[6] = KnowCase("fuego", "fok")
+            knownWords[7] = KnowCase("hombre", "hom")
+            knownWords[8] = KnowCase("indice", "inodex")
+            knownWords[9] = KnowCase("alkaesto", "OtoseHkL")
+            knownWords[10] = KnowCase("personajes", "puerusoNtujes")
+            knownWords[11] = KnowCase("abrir", "rirubo")
+            knownWords[12] = KnowCase("templo", "erupmet")
+            knownWords[13] = KnowCase("tierra", "tera")
+
+            return knownWords
+        }
+    }
+}
+
--- app/src/main/java/diphendara/antiguo/translator/parsers/Text2Antiguo.kt
+++ app/src/main/java/diphendara/antiguo/translator/parsers/Text2Antiguo.kt
@@ -32,7 +32,7 @@ class Text2Antiguo {
         {
             var cleanText = inputText
 
-            for (word in getKnownWords()) {
+            for (word in KnowCase.getKnownWords()) {
                 cleanText = cleanText.replace(word.value, word.antiguoValue)
             }
 
@@ -90,29 +90,5 @@ class Text2Antiguo {
             return cleanText
         }
 
-        private fun getKnownWords(): Array<KnowCase>
-        {
-
-            // https://cincoelementos.fandom.com/es/wiki/Lenguaje_Antiguo#Diccionario
-            val knownWords = Array(14) { KnowCase("Agua", "HguH") }
-
-            // Read the README to know why this values are like this
-            knownWords[0]  = KnowCase("agua", "HiW")
-            knownWords[1]  = KnowCase("argumento", "Hrugumentu")
-            knownWords[2]  = KnowCase("capitulo", "Cpitoru")
-            knownWords[3]  = KnowCase("mujer", "DoN")
-            knownWords[4]  = KnowCase("aire", "Er")
-            knownWords[5]  = KnowCase("espacio", "esPi")
-            knownWords[6]  = KnowCase("fuego", "fok")
-            knownWords[7]  = KnowCase("hombre", "hom")
-            knownWords[8]  = KnowCase("indice", "inodex")
-            knownWords[9]  = KnowCase("alkaesto", "OtoseHkL")
-            knownWords[10] = KnowCase("personajes", "puerusoNtujes")
-            knownWords[11] = KnowCase("abrir", "rirubo")
-            knownWords[12] = KnowCase("templo", "erupmet")
-            knownWords[13] = KnowCase("tierra", "tera")
-
-            return knownWords
-        }
     }
 }
